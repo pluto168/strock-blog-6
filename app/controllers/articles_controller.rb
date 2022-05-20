8c578cd8ed2a501306b447a7a60aff4b197a8545
Pluto <pluto88.cloud@gmail.com>
2022-05-20 22:29:03 +0800

    notice massages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,4 +5,20 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+    def new
+        @article = Article.new
+    end
+
+    def create
+        #rander plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title,:description))
+        #render plain: @article.inspect
+        if @article.save
+            flash[:notice] = "Article was creared successfully!"
+            #redirect_to article_path(@article)
+            redirect_to @article
+        else
+            render 'new' #重新填表單
+        end
+    end
 end
