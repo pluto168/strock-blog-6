90ec895e7b192a33a1f35c20e7bc3c5b9dc29d4c
pluto168 <baymax1100@gmail.com>
2022-05-30 00:44:02 +0800

    Add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,9 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user =Users.new(user_params)
+        @user =User.new(user_params)
         if @user.save
-            flash[:notice] = "Welcome to the Strock Blog, you have successfully signed up."
+            flash[:notice] = "Welcome to the Strock Blog #{@user.username}, you have successfully signed up."
             redirect_to articles_path
         else 
             render 'new'
