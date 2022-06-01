a3a8abe01779c475321b8bce369c43996f57b5df
pluto168 <baymax1100@gmail.com>
2022-06-01 19:03:24 +0800

    require_same_user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,10 @@
 class ArticlesController < ApplicationController
 
-    before_action :set_article, only:[:show, :edit, :update, :destroy]
     #執行操作前，先到set
+    before_action :set_article, only:[:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
 
     def show
         # redirect_to article_path(@article)
@@ -56,5 +59,12 @@ class ArticlesController < ApplicationController
 
     def article_params
         params.require(:article).permit(:title,:description)
+    end
+
+    def require_same_user
+        if current_user != @article.user
+            flash[:alert] = "You can only edit or delete your own article"
+            redirect_to @article
+        end
     end
 end
